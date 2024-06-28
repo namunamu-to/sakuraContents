f2c1828984147a11d4f7f47e83e0ba5bf3c8681c
namunamu-to <ten1baniraSS@gmail.com>
2024-06-28 16:05:10 +0900

    描画処理の修正

--- games/othello/js/gameLib.js
+++ games/othello/js/gameLib.js
@@ -2,8 +2,7 @@ const body = document.body;
 
 //canvas要素追加
 (function () {
-    const addHtml = `<canvas id="canvasElm"></canvas>`;
-    body.innerHTML += addHtml;
+    body.innerHTML += `<canvas id="canvasElm"></canvas>`;
 }());
 
 //canvasの定義
@@ -27,40 +26,10 @@ function drawLine(color, x, y, x1, y1) {
 setInterval(() => { //描画更新
     canvasElm.width = window.innerWidth;
     canvasElm.height = window.innerHeight;
+    ctx.clearRect(0, 0, canvasElm.width, canvasElm.height) //キャンバスをクリア
+    
     for(let key of Object.keys(draws)){
         draws[key]();
-        console.log(draws[key]);
     }
 
 }, 33); //30fps
-
-
-//dialogの設定
-(function () {
-    const style = `
-        padding: 1rem;
-        font-size: 3rem;
-        width: fit-content;
-        height: fit-content;
-        background-color: #fff;
-        margin: auto;
-        display: none;
-        text-align: center;
-        top: 0;
-        left: 0;
-    `;
-
-    const addHtml = `<div class="position-fixed border border-dark dialogElm" id="showDialogElm" style="${style}"></div>`;
-    body.innerHTML += addHtml;
-}());
-
-
-function dialog(innerHTML = "") {
-    if (innerHTML == "") {
-        showDialogElm.style.display = "none";
-        return;
-    }
-
-    showDialogElm.style.display = "block";
-    showDialogElm.innerHTML = innerHTML;
-}
--- games/othello/js/script.js
+++ games/othello/js/script.js
@@ -1,19 +1,20 @@
 //タイルの定義
 draws.background = () => { //背景描画
-    fillRect("green", 0, 0, canvasElm.width, canvasElm.width);
+    fillRect("#aaa", 0, 0, canvasElm.width, canvasElm.width);
 }
 
 
-draws.tile = () => { //マス目描画
+draws.tile = () => { //ボード描画
     let sideTileNum = 8; //値が8なら8x8のマスが生まれる
     const boardSize = Math.min(canvasElm.width, canvasElm.height) * 0.95;
-    let tileSize = boardSize / sideTileNum;
-
+    const tileSize = boardSize / sideTileNum;
+    const startX = (canvasElm.width - boardSize) / 2;
+    const startY = (canvasElm.height - boardSize) / 2;
+    
     for (let i = 0; i < sideTileNum + 1; i++) {
-        drawLine("#fff", 0, tileSize * i, canvasElm.width, tileSize * i); //縦ラインを描画
-        drawLine("#fff", tileSize * i, 0, tileSize * i, canvasElm.width); //横ラインを描画     
+        const offset = tileSize * i;
+
+        drawLine("#fff", 0, offset, boardSize, offset); //横ラインを描画
+        drawLine("#fff", offset, 0, offset, boardSize); //縦ラインを描画     
     }
-
 }
-
-// dialog("テスト");
