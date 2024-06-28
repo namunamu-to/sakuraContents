c4fc70b424acaa110d8b5d2191a8da5f2328e3e5
namunamu-to <ten1baniraSS@gmail.com>
2024-06-28 16:32:31 +0900

    ボードを正しく表示できるよう変更

--- games/othello/js/script.js
+++ games/othello/js/script.js
@@ -4,17 +4,20 @@ draws.background = () => { //背景描画
 }
 
 
-draws.tile = () => { //ボード描画
+draws.board = () => { //ボード描画
     let sideTileNum = 8; //値が8なら8x8のマスが生まれる
     const boardSize = Math.min(canvasElm.width, canvasElm.height) * 0.95;
     const tileSize = boardSize / sideTileNum;
-    const startX = (canvasElm.width - boardSize) / 2;
-    const startY = (canvasElm.height - boardSize) / 2;
-    
+    const baseX = (canvasElm.width - boardSize) / 2;
+    const baseY = (canvasElm.height - boardSize) / 2;
+
+    fillRect("green", baseX, baseY, boardSize, boardSize); //ボードの背景描画
+
+    //マス目描画
     for (let i = 0; i < sideTileNum + 1; i++) {
         const offset = tileSize * i;
 
-        drawLine("#fff", 0, offset, boardSize, offset); //横ラインを描画
-        drawLine("#fff", offset, 0, offset, boardSize); //縦ラインを描画     
+        drawLine("#fff", baseX, baseY+offset, baseX+boardSize, baseY+offset); //横ラインを描画
+        drawLine("#fff", baseX+offset, baseY, baseX+offset, baseY+boardSize); //縦ラインを描画     
     }
 }
