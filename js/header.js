dd99b9be87e23916320d251894684c956170de94
namunamu-to <ten1baniraSS@gmail.com>
2024-07-26 15:15:00 +0900

    サイトの共通化

--- js/header.js
+++ js/header.js
@@ -1,45 +1,68 @@
 (function () {
     let addHtml = ` 
-    
-    <nav id="headerNavElm" class="mainBgColor nav py-3 mb-5 ">
-        <span></span>
-        <img id="headerLogo" src="https://galleon.yachiyo.tech/ROGO.png" alt="ロゴ">
-        <span>
-            <a class="navLink" href="https://galleon.yachiyo.tech/index.html">HOME</a>
-        </span>
+    <nav id="headerElm">
+        <img id="headerLogo" src="./ROGO.png" alt="ロゴ">
+
+        <span id="headerNavElm"></span>
     </nav>
     
     
 
     <style>
-        #headerNavElm {
+        #headerElm {
+            font-size: 16px;
             height: 10rem;
             gap: 2rem;
             color: #fff;
-            font-size: 2rem;
+            display: flex;
             align-items: center;
+            padding: 1rem;
+            background-color: #000;
         }
-
-            
-        #headerNavElm a {
-            text-decoration:none;
+        
+        #headerElm a {
+            text-decoration: none;
             color: #fff;
-            border-bottom: solid 3px #66deff;
         }
-
+        
+        #headerElm a:hover {
+            opacity: 0.5;
+        }
+        
         #headerLogo {
             height: 100%;
         }
-
-        .mainBgColor {
-            background-color: #000;
+        
+        #headerNavElm {
+            font-size: 2rem;
+            display: flex;
+            flex-wrap: wrap;
+            gap: 1rem;
         }
+        
+        #headerNavElm>* {
+            margin-left: 2rem;
+        }
+        
+        .navNowPage {
+            border-bottom: solid 3px #a9dff3;
+            font-weight: bold;
+        }
+    
     </style>
     `;
 
-    const urls = {
-        
+    document.body.innerHTML += addHtml;
+
+    let files = {
+        "index.html": "HOME",
+        "games.html": "ゲーム",
+        "blogs.html": "ブログ",
     }
 
-    document.body.innerHTML += addHtml;
+    let nowFile = window.location.href.split("/").pop();
+    for (let key of Object.keys(files)){
+        const nowPageClass = nowFile == key ? "navNowPage" : "";
+        headerNavElm.innerHTML += `<span><a class="navLink ${nowPageClass}" href="${"https://galleon.yachiyo.tech/" + key}">${files[key]}</a></span>`;
+    }
 }())
