56b8d95e84c7fe97f085d25fb8c19e03ca11a16f
namunamu-to <ten1baniraSS@gmail.com>
2025-08-29 09:52:07 +0900

    header共通化

---
+++ js/header copy.js
@@ -0,0 +1,68 @@
+(function () {
+    let addHtml = ` 
+    <header id="headerElm" class="header-flex">
+        <img id="headerLogo" src="/ROGO.png" alt="ロゴ">
+        <span id="headerNavElm" class="header-flex"></span>
+    </header>
+    
+    <style>
+        .header-flex {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        #headerElm {
+            font-size: 1rem;
+            color: #fff;
+            align-items: center;
+            background-color: #000;
+            width: auto;
+            height: fit-content;
+            gap: 1rem;
+        }
+
+        #headerElm a {
+            text-decoration: none;
+            color: #fff;
+        }
+
+        #headerElm a:hover {
+            opacity: 0.5;
+        }
+
+        #headerLogo {
+            height: 8rem;
+            width: auto;
+            margin: 1rem;
+        }
+
+        #headerNavElm {
+            font-size: 2rem;
+            gap: 2rem;
+            margin: 1rem;
+            justify-content: center;
+        }
+
+        .navNowPage {
+            border-bottom: solid 3px #a9dff3;
+            font-weight: bold;
+        }
+    </style>
+    `;
+
+    document.body.innerHTML += addHtml;
+
+    let files = {
+        "works.html": "HOME",
+        "games.html": "ゲーム",
+        "tools.html": "ツール",
+        "blogs.html": "ブログ",
+        "index.html": "会社概要",
+    }
+
+    let nowFile = window.location.href.split("/").pop();
+    for (let key of Object.keys(files)){
+        const nowPageClass = nowFile == key ? "navNowPage" : "";
+        headerNavElm.innerHTML += `<span><a class="${nowPageClass}" href="${"/" + key}">${files[key]}</a></span>`;
+    }
+}())
--- js/header.js
+++ js/header.js
@@ -1,68 +1,93 @@
 (function () {
     let addHtml = ` 
-    <header id="headerElm" class="header-flex">
-        <img id="headerLogo" src="/ROGO.png" alt="ロゴ">
-        <span id="headerNavElm" class="header-flex"></span>
-    </header>
-    
-    <style>
-        .header-flex {
-            display: flex;
-            flex-wrap: wrap;
-        }
+        <header id="header">
+            <nav>
+                <ul id="headerNavElm">
+                </ul>
+            </nav>
+        </header>
+        
+        <style>
+            #header {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: flex-end;
+                background: rgba();
+            }
 
-        #headerElm {
-            font-size: 1rem;
-            color: #fff;
-            align-items: center;
-            background-color: #000;
-            width: auto;
-            height: fit-content;
-            gap: 1rem;
-        }
+            #header.scrolled {
+                background-color: #212121;
+            }
 
-        #headerElm a {
-            text-decoration: none;
-            color: #fff;
-        }
+            #header.scrolled .now-page {
+                color: rgba(125, 182, 177, 1);
+            }
 
-        #headerElm a:hover {
-            opacity: 0.5;
-        }
+            #header nav {
+                padding: 1rem;
+            }
 
-        #headerLogo {
-            height: 8rem;
-            width: auto;
-            margin: 1rem;
-        }
 
-        #headerNavElm {
-            font-size: 2rem;
-            gap: 2rem;
-            margin: 1rem;
-            justify-content: center;
-        }
 
-        .navNowPage {
-            border-bottom: solid 3px #a9dff3;
-            font-weight: bold;
-        }
-    </style>
+            #header nav ul {
+                list-style: none;
+                margin: 0;
+                padding: 0;
+                display: flex;
+            }
+
+
+            /* 初期リンクの色 */
+                #header nav ul li a {
+                color: rgba(0, 0, 0, 0.5);
+                text-decoration: none;
+                padding: 10px 15px;
+                transition: color 0.5s ease;
+            }
+
+            /* スクロール後の色 */
+            #header.scrolled nav ul li a {
+                color: #fff;
+            }
+
+            #header.scrolled nav ul li .nowPage {
+                color: #7db6b1;
+            }
+        </style>
     `;
 
     document.body.innerHTML += addHtml;
 
     let files = {
-        "works.html": "HOME",
-        "games.html": "ゲーム",
-        "tools.html": "ツール",
-        "blogs.html": "ブログ",
         "index.html": "会社概要",
+        "unilink.html": "UniLinkの紹介",
+        "appIntro.html": "アプリの紹介",
+        "aboutYachiyo.html": "八千代技研について",
+        "humanResourceRecruit.html": "人材募集",
+        "lab.html": "ラボ",
     }
 
     let nowFile = window.location.href.split("/").pop();
-    for (let key of Object.keys(files)){
-        const nowPageClass = nowFile == key ? "navNowPage" : "";
-        headerNavElm.innerHTML += `<span><a class="${nowPageClass}" href="${"/" + key}">${files[key]}</a></span>`;
+    console.log(nowFile);
+
+    for (let key of Object.keys(files)) {
+        const nowPageClass = nowFile == key ? "nowPage" : "";
+        headerNavElm.innerHTML += `
+            <li><a class="${nowPageClass}" href="${"./" + key}">${files[key]}</a></li>                
+        `;
     }
+
+
+    window.addEventListener('scroll', function () {
+        var header = document.getElementById('header');
+        if (window.scrollY > 50) { // スクロール位置が50pxを超えたら
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
 }())
